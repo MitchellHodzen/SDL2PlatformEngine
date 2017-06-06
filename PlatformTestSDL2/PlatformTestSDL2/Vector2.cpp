ceb557e76e76ab883f9a8af688b07295971e30b3
Mitchell Hodzen <MitchellHodzen@yahoo.com>
2017-06-06 11:49:55 -0400

    Added first iteration of vector2 class

---
+++ PlatformTestSDL2/PlatformTestSDL2/Vector2.cpp
@@ -0,0 +1,44 @@
+#include "Vector2.h"
+
+
+Vector2::Vector2(float x, float y)
+{
+	//If no value is given, X and Y default to 0
+	internalArray[0] = x;
+	internalArray[1] = y;
+}
+
+Vector2::~Vector2()
+{
+	delete [] internalArray;
+}
+
+float Vector2::GetX()
+{
+	return internalArray[0];
+}
+
+float Vector2::GetY()
+{
+	return internalArray[1];
+}
+
+void Vector2::SetX(float x)
+{
+	internalArray[0] = x;
+}
+
+void Vector2::SetY(float y)
+{
+	internalArray[1] = y;
+}
+
+bool Vector2::CheckEqual(Vector2& v1, Vector2& v2)
+{
+	if (v1.GetX() == v2.GetX() && v1.GetY() == v2.GetY())
+	{
+		return true;
+	}
+	return false;
+}
+
---
+++ PlatformTestSDL2/PlatformTestSDL2/Vector2.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Vector2
+{
+public:
+	Vector2(float x = 0.0f, float y = 0.0f);
+	~Vector2();
+	float GetX();
+	float GetY();
+	void SetX(float x);
+	void SetY(float y);
+	static bool CheckEqual(Vector2& v1, Vector2& v2);
+private:
+	float* internalArray = new float[2];
+};
