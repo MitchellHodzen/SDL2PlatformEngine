1402d401bdcc869e53801256adaa985f57d106a1
Mitchell Hodzen <MitchellHodzen@yahoo.com>
2017-06-08 14:26:23 -0400

    Changed entity position handling from two floats to new Vector2 class

--- PlatformTestSDL2/PlatformTestSDL2/Entity.cpp
+++ PlatformTestSDL2/PlatformTestSDL2/Entity.cpp
@@ -4,78 +4,51 @@
 
 Entity::Entity(float x, float y, int collWidth, int collHeight, float collXOffset, float collYOffset, bool isTrigger)
 {
-	//rect = new SDL_Rect{ (int)x, (int)y, width, height };
-	//this->collSizeOffset = collOffset * 2;
 	col = new Collider(x + collXOffset, y + collYOffset, collWidth, collHeight, isTrigger);
-	posX = x;
-	posY = y;
-	//this->width = width;
-	//this->height = height;
+	position = new Vector2(x, y);
 	this->collXOffset = collXOffset;
 	this->collYOffset = collYOffset;
-	//std::cout << GetCollider()->GetWidth() << std::endl;
 }
 
 
 Entity::~Entity()
 {
-	//delete rect;
-	//rect = nullptr;
 	delete col;
 	col = nullptr;
-	//delete animationController;
-	//animationController = nullptr;
 	delete visualComponent;
 	visualComponent = nullptr;
+	delete position;
+	position = nullptr;
 }
 
-//void Entity::Render(SDL_Renderer* renderer)
-//{
-//	if (texture != nullptr)
-//	{
-//		texture->Render((int)posX, (int)posY, renderer);
-//	}
-//	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-//	//(renderer, GetCollider()->GetRect()); //Debug: Draws the colliders
-//}
 
 void Entity::SetPosX(float x)
 {
-	posX = x;
+	position->SetX(x);
 	col->SetPosX(x + collXOffset);
 }
 void Entity::SetPosY(float y)
 {
-	posY = y;
+	position->SetY(y);
 	col->SetPosY(y + collYOffset);
 }
 void Entity::SetColliderWidth(int width)
 {
-	//this->width = width;
 	col->SetWidth(width);
 }
 void Entity::SetColliderHeight(int height)
 {
-	//this->height = height;
 	col->SetHeight(height);
 }
 
 float Entity::GetPosX()
 {
-	return posX;
+	return position->GetX();
 }
 float Entity::GetPosY()
 {
-	return posY;
+	return position->GetY();
 }
-//int Entity::GetWidth()
-//{
-//	return width;
-//}
-//int Entity::GetHeight()
-//{
-//	return height;
-//}
 float Entity::GetCollXOffset()
 {
 	return collXOffset;
@@ -88,30 +61,6 @@ Collider* Entity::GetCollider()
 {
 	return col;
 }
-//SDL_Rect* Entity::GetRect()
-//{
-//	return rect;
-//}
-
-//void Entity::ResetRect()
-//{
-//	delete rect;
-//	rect = new SDL_Rect{ (int)posX, (int)posY, width, height };
-//}
-
-//void Entity::SetTexture(Texture* texture)
-//{
-//	this->texture = texture;
-//}
-//Texture* Entity::GetTexture()
-//{
-//	return texture;
-//}
-
-//AnimationController* Entity::GetAnimationController()
-//{
-//	return animationController;
-//}
 VisualComponent* Entity::GetVisualComponent()
 {
 	return visualComponent;
--- PlatformTestSDL2/PlatformTestSDL2/Entity.h
+++ PlatformTestSDL2/PlatformTestSDL2/Entity.h
@@ -4,13 +4,13 @@
 #include"Collider.h"
 #include "Texture.h"
 #include "VisualComponent.h"
+#include "Vector2.h"
 class Entity
 {
 public:
 	Entity(float x, float y, int collWidth, int collHeight, float collXOffset, float collYOffset, bool isTrigger);
 	~Entity();
 
-	//void Render(SDL_Renderer* renderer);
 	void SetPosX(float x);
 	void SetPosY(float y);
 	void SetColliderWidth(int width);
@@ -18,31 +18,19 @@ public:
 
 	float GetPosX();
 	float GetPosY();
-	//int GetWidth();
-	//int GetHeight();
 	float GetCollXOffset();
 	float GetCollYOffset();
 	Collider* GetCollider();
-	//void SetTexture(Texture* texture);
-	//Texture* GetTexture();
-	//AnimationController* GetAnimationController();
 	VisualComponent* GetVisualComponent();
-	//SDL_Rect* GetRect(); 
 
 private:
-	//SDL_Rect* rect;
+	Vector2* position;
 	Collider* col;
-	float posX;
-	float posY;
-	//int width;
-	//int height;
+
 	float collXOffset;
 	float collYOffset;
 	float collSizeOffset;
 
-	//Texture* texture;
-	//void ResetRect();
-	//AnimationController* animationController = new AnimationController();
 protected:
 	VisualComponent* visualComponent;
 };
